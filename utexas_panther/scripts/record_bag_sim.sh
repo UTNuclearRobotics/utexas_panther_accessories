180798139338441d14ffdc5b182b9be12783134c
Andrew Sharp <asharp@utexas.edu>
2025-02-06 19:42:04 -0600

    Changing to use the Zed topics.

--- utexas_panther/scripts/record_bag_sim.sh
+++ utexas_panther/scripts/record_bag_sim.sh
@@ -3,7 +3,9 @@ BAG_NAME="panther_recording_$(date +%Y%m%d_%H%M%S)"
 echo "Recording bag file..."
 echo "Outputting bag to ${HOME}/ros2_ws/src/data/$BAG_NAME"
 ros2 bag record -o ${HOME}/ros2_ws/src/data/$BAG_NAME \
-    /camera/color/image_raw \
-    /camera/color/camera_info \
+    /zed/zed_node/rgb/camera_info \
+    /zed/zed_node/rgb/image_rect_color \
     /imu/data \
     /odometry/filtered
+    #/camera/color/image_raw \
+    #/camera/color/camera_info \
