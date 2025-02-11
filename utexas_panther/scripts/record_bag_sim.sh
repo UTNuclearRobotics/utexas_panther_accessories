9987d347b6707f06209ab2bd942307c445c2b465
Andrew Sharp <asharp@utexas.edu>
2025-02-10 21:26:48 -0600

    Moving depthai to an internal fork.

--- utexas_panther/scripts/record_bag_sim.sh
+++ utexas_panther/scripts/record_bag_sim.sh
@@ -3,8 +3,8 @@ BAG_NAME="panther_recording_$(date +%Y%m%d_%H%M%S)"
 echo "Recording bag file..."
 echo "Outputting bag to ${HOME}/ros2_ws/src/data/$BAG_NAME"
 ros2 bag record -o ${HOME}/ros2_ws/src/data/$BAG_NAME \
-    /zed/zed_node/rgb/camera_info \
-    /zed/zed_node/rgb/image_rect_color \
+    /oak/oak_d_node/rgb/camera_info \
+    /oak/oak_d_node/rgb/image_rect_color \
     /imu/data \
     /odometry/filtered
     #/camera/color/image_raw \
