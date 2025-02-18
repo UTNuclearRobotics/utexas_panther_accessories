4547372a65f19d0b2290dab4840277c660815664
Nick Chan <nick.chan@utexas.edu>
2025-02-18 10:19:48 -0600

    Add new sim camera topics

--- utexas_panther/scripts/record_bag_sim.sh
+++ utexas_panther/scripts/record_bag_sim.sh
@@ -3,8 +3,9 @@ BAG_NAME="panther_recording_$(date +%Y%m%d_%H%M%S)"
 echo "Recording bag file..."
 echo "Outputting bag to ${HOME}/ros2_ws/src/data/$BAG_NAME"
 ros2 bag record -o ${HOME}/ros2_ws/src/data/$BAG_NAME \
-    /oak/oak_d_node/rgb/camera_info \
-    /oak/oak_d_node/rgb/image_rect_color \
+    /oakd1/oak_d_node/rgb/image_rect_color \
+    /oakd2/oak_d_node/rgb/image_rect_color \
+    /oakd3/oak_d_node/rgb/image_rect_color \
     /imu/data \
     /odometry/filtered
     #/camera/color/image_raw \
