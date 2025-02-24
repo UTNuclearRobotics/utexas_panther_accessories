b4557df99645c2dec6d3af4eb9bbdff8deb3e56a
Andrew Sharp <asharp@utexas.edu>
2025-02-23 22:55:56 -0600

    Updating sim bag topics.

--- utexas_panther/scripts/record_bag_sim.sh
+++ utexas_panther/scripts/record_bag_sim.sh
@@ -3,9 +3,8 @@ BAG_NAME="panther_recording_$(date +%Y%m%d_%H%M%S)"
 echo "Recording bag file..."
 echo "Outputting bag to ${HOME}/ros2_ws/src/data/$BAG_NAME"
 ros2 bag record -o ${HOME}/ros2_ws/src/data/$BAG_NAME \
-    /oakd1/oak_d_node/rgb/image_rect_color \
+    /oakd2/oak_d_node/rgb/camera_info \
     /oakd2/oak_d_node/rgb/image_rect_color \
-    /oakd3/oak_d_node/rgb/image_rect_color \
     /imu/data \
     /odometry/filtered
     #/camera/color/image_raw \
