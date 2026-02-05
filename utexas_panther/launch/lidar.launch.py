d67f3c7960090b5eb3fe5a82e47a67b8920b95bc
ryanl0518 <ryanlee0518@utexas.edu>
2026-02-05 16:35:48 -0600

    added lidar params and updated launch file to use driver.launch.py

--- utexas_panther/launch/lidar.launch.py
+++ utexas_panther/launch/lidar.launch.py
@@ -15,7 +15,7 @@ def launch_setup(context, *args, **kwargs):
     # ros2 launch ouster_ros sensor.launch.xml sensor_hostname:=192.168.1.100 viz:=false
     lidar_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [PathJoinSubstitution([FindPackageShare("ouster_ros"), "launch", "sensor.composite.launch.py"])]
+            [PathJoinSubstitution([FindPackageShare("ouster_ros"), "launch", "driver.launch.py"])]
         ),
         launch_arguments={
             "params_file": PathJoinSubstitution([
@@ -24,7 +24,6 @@ def launch_setup(context, *args, **kwargs):
                 "ouster_params.yaml"
             ]),
             "viz": "false",
-            "ouster_ns": "panther"
         }.items(),
     )
 
