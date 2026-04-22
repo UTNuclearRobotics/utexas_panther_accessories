80c3c9067d980ae916ac7a417346186c21d5e400
Andrew Sharp <asharp@utexas.edu>
2026-04-22 15:15:59 -0500

    Changing everything to sim time.

--- utexas_panther/launch/bringup.launch.py
+++ utexas_panther/launch/bringup.launch.py
@@ -115,7 +115,7 @@ def generate_launch_description():
     )
     declare_use_sim_time_arg = DeclareLaunchArgument(
         "use_sim_time",
-        default_value="false",
+        default_value="true",
         description="Use simulation (Gazebo) clock if true.",
     )
 
