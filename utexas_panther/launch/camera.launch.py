71d6adb6d9ed7f3357c5afdafcf8cd9a148cf343
Andrew Sharp <asharp@utexas.edu>
2026-04-30 13:21:18 -0500

    Changes to get the camera working.

--- utexas_panther/launch/camera.launch.py
+++ utexas_panther/launch/camera.launch.py
@@ -19,7 +19,7 @@ def launch_setup(context, *args, **kwargs):
     # ros2 launch depthai_ros_driver camera.launch.py
     depthai_camera = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [PathJoinSubstitution([FindPackageShare("depthai_ros_driver"), "launch", "camera.launch.py"])]
+            [PathJoinSubstitution([FindPackageShare("depthai_ros_driver_v3"), "launch", "driver.launch.py"])]
         ),
         launch_arguments={"camera_i_restart_on_diagnostics_error": "true"}.items(),
     )
